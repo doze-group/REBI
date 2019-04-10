dd155def024e45269f2f2b1d2f6b68278c29e87b
DevelopGadget <fernandoaraujo081@gmail.com>
2019-04-10 17:32:44 -0500

    [DevelopGadget] [mié._10_04] Clases

--- web/React/Components/NavBar/NavBar.js
+++ web/React/Components/NavBar/NavBar.js
@@ -1,81 +1,99 @@
 const { Component } = React;
-const { Redirect } = ReactRouterDOM;
 
-function NavBar() {
-    return (
-        <nav class="navbar is-fixed-top level is-fixed-top has-background-light">
-            <div class="level-left Logo">
-                <div class="level-item">
-                    <img src="https://tecnologicocomfenalco.edu.co/sites/default/files/Logo.png" alt="Logo" />
+class NavBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { Login }
+    }
+
+    SelectBox() {
+        const Items = [
+            {
+                Title: 'Tu Perfil',
+                icon: 'fa-user-circle'
+            },
+            {
+                Title: 'Tus Archivos',
+                icon: 'fa-file-archive'
+            },
+            {
+                Title: 'Inicio',
+                icon: 'fa-home'
+            },
+            {
+                Title: 'Configuración',
+                icon: 'fa-cogs'
+            },
+            {
+                Title: 'Salir',
+                icon: 'fa-sign-out-alt'
+            }
+        ];
+
+        return (
+            <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div class="dropdown-content">
+                    {
+                        Items.map((item, i) => {
+                            return (
+                                <div>
+                                    {i !== 0 ? <hr class="dropdown-divider" /> : null}
+                                    <a class="dropdown-item has-text-centered is-paddingless">
+                                        <span class="icon is-medium has-text-info">
+                                            <i class={"fas " + item.icon + " fa-2x"}></i>
+                                        </span>
+                                        <p>
+                                            {item.Title}
+                                        </p>
+                                    </a>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
-            <div class="level-right">
-                <div class="level-item">
-                    <div class="dropdown is-hoverable is-left is-hidden-mobile NavDrop">
-                        <div class="dropdown-trigger">
-                            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
-                                <span class="image is-24x24">
-                                    <figure>
-                                        <img src="http://www.iconarchive.com/download/i60041/mattahan/ultrabuuf/Comics-Spiderman-Cam.ico" />
-                                    </figure>
-                                </span>
-                                <span class="Nombre">Fernando Araujo</span>
-                                <span class="icon is-small">
-                                    <i class="fas fa-angle-down" aria-hidden="true"></i>
-                                </span>
-                            </button>
-                        </div>
-                        <div class="dropdown-menu" id="dropdown-menu3" role="menu">
-                            <div class="dropdown-content">
-                                <a class="dropdown-item has-text-centered is-paddingless">
-                                    <span class="icon is-medium has-text-info">
-                                        <i class="fas fa-user-circle fa-2x"></i>
-                                    </span>
-                                    <p>
-                                        Tu Perfil
-                                    </p>
-                                </a>
-                                <hr class="dropdown-divider" />
-                                <a class="dropdown-item has-text-centered is-paddingless">
-                                    <span class="icon is-medium has-text-info">
-                                        <i class="fas fa-file-archive fa-2x"></i>
-                                    </span>
-                                    <p>
-                                        Tus Archivos
-                                    </p>
-                                </a>
-                                <hr class="dropdown-divider" />
-                                <a class="dropdown-item has-text-centered is-paddingless">
-                                    <span class="icon is-medium has-text-info">
-                                        <i class="fas fa-home fa-2x"></i>
-                                    </span>
-                                    <p>
-                                        Inicio
-                                    </p>
-                                </a>
-                                <hr class="dropdown-divider" />
-                                <a class="dropdown-item has-text-centered is-paddingless">
-                                    <span class="icon is-medium has-text-info">
-                                        <i class="fas fa-cogs fa-2x"></i>
-                                    </span>
-                                    <p>
-                                        Configuración
-                                    </p>
-                                </a>
-                                <hr class="dropdown-divider" />
-                                <a class="dropdown-item has-text-centered is-paddingless">
-                                    <span class="icon is-medium has-text-danger">
-                                        <i class="fas fa-sign-out-alt fa-2x"></i>
-                                    </span>
-                                    <p>
-                                        Salir
-                                    </p>
-                                </a>
-                            </div>
-                        </div>
+        );
+    }
+
+    render() {
+        return (
+            <nav class="navbar is-fixed-top level is-fixed-top has-background-light">
+                <div class="level-left Logo">
+                    <div class="level-item">
+                        <img src="https://tecnologicocomfenalco.edu.co/sites/default/files/Logo.png" alt="Logo" />
                     </div>
                 </div>
-            </div>
-        </nav>
-    );
+                <div class="level-right">
+                    <div class="level-item NavDrop">
+                        {this.state.Login != null ?
+                            <div>
+                                <div class="dropdown is-hoverable is-left is-hidden-mobile">
+                                    <div class="dropdown-trigger">
+                                        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+                                            <span class="image is-24x24">
+                                                <figure>
+                                                    <img src="http://www.iconarchive.com/download/i60041/mattahan/ultrabuuf/Comics-Spiderman-Cam.ico" />
+                                                </figure>
+                                            </span>
+                                            <span class="Nombre">Fernando Araujo</span>
+                                            <span class="icon is-small">
+                                                <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                            </span>
+                                        </button>
+                                    </div>
+                                    {this.SelectBox()}
+                                </div>
+                            </div>
+                            : <button class="button is-link is-fullwidth">
+                                <span class="icon is-small">
+                                    <i class="fas fa-sign-in-alt"></i>
+                                </span>
+                                <span>Login</span>
+                            </button>}
+                    </div>
+                </div>
+            </nav>
+        );
+    }
 }
